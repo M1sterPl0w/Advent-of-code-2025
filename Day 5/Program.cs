46257264936524438943be65deff8f05cc625856
= <=>
2025-12-05 12:39:47 +0100

    feature(app): day-5 Day 5 part one and two

---
+++ Day 5/Program.cs
@@ -0,0 +1,9 @@
+﻿using Day_5;
+
+var result = FoodChecker.FreshFood();
+Console.WriteLine($"Amount of fresh foods: {result}");
+
+var resultPartTwo = FoodChecker.CountPossibleFoodIds();
+Console.WriteLine($"Amount of possible food ids: {resultPartTwo}");
+
+Console.ReadKey();
